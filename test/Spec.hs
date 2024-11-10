6205f6c4cf1cf2a6ae2b9002d0dc8f74de523e36
Aurelijus Lukšas <aurelijus.luksas@mif.stud.vu.lt>
2024-11-11 00:52:17 +0200

    unit tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,66 +1,110 @@
 {-# LANGUAGE ImportQualifiedPost #-}
-import Test.Tasty ( TestTree, defaultMain, testGroup )
-import Test.Tasty.HUnit ( testCase, (@?=) )
+
+import Test.Tasty (TestTree, defaultMain, testGroup)
+import Test.Tasty.HUnit (testCase, (@?=))
 
 import Lib1 qualified
 import Lib2 qualified (
-        Query(..),
-      State(..),
-      Name(..),
-      parseQuery,
-      emptyState,
-      stateTransition,
-      parseName,
-      parseQuantity,
-      parseUnit,
-      parseIngredient,
-      parseIngredientList,
-      parseCreate,
-      parseCreateList,
-      parseAdd,
-      parseRemove,
-      parseGet,
-      parseSelect,
-      parseUpdate,
-      parseDelete
-      )
+    Query(..),
+    State(..),
+    Name(..),
+    Quantity(..),
+    Unit(..),
+    Ingredient(..),
+    IngredientList(..),
+    parseQuery,
+    emptyState,
+    stateTransition,
+    parseName,
+    parseQuantity,
+    parseUnit,
+    parseIngredient,
+    parseIngredientList,
+    parseCreate,
+    parseCreateList,
+    parseAdd,
+    parseRemove,
+    parseGet,
+    parseSelect,
+    parseUpdate,
+    parseDelete
+    )
 
 main :: IO ()
 main = defaultMain tests
 
 tests :: TestTree
-tests = testGroup "Tests" [unitTests]
+tests = testGroup "Lib2 Tests"
+  [ unitTests
+  , parserTests
+  , stateTransitionTests
+  ]
 
+-- Unit Tests for Parser Functions
 unitTests :: TestTree
-unitTests = testGroup "Lib Tests"
-  [ testGroup "Lib2 parseQuery tests"
-      [ testCase "Parse empty input should give error" $
-          parseQuery "" @?= Left "Input received:  | Parse errors: Expected 'create(', Expected 'add(', Expected 'remove(', Expected 'get(', Expected 'select(', Expected 'update(', Expected 'create_list(', Expected 'delete('"
-      ],
-    testGroup "Lib2 parseUnit tests"
-      [ testCase "Parse 'cup' unit" $
-          parseUnit "cup" @?= Right (Cup, "")
-      , testCase "Parse 'tbsp' unit" $
-          parseUnit "tbsp" @?= Right (Tbsp, "")
-      , testCase "Parse invalid unit" $
-          parseUnit "invalid" @?= Left "Invalid unit"
-      ],
-    testGroup "Lib2 stateTransition tests"
-      [ testCase "Create ingredient" $
-          stateTransition emptyState (Create (WordName "Sugar") (Quantity 1) Cup) @?= Right (["Created ingredient"], State [] [Ingredient (WordName "Sugar") (Quantity 1) Cup])
-      , testCase "Add ingredient" $
-          stateTransition (State [] [Ingredient (WordName "Sugar") (Quantity 1) Cup]) (Add (WordName "Sugar")) @?= Right (["Added ingredient"], State [] [Ingredient (WordName "Sugar") (Quantity 1) Cup, Ingredient (WordName "Sugar") (Quantity 1) Cup])
-      , testCase "Remove ingredient" $
-          stateTransition (State [] [Ingredient (WordName "Sugar") (Quantity 1) Cup]) (Remove (WordName "Sugar")) @?= Right (["Removed ingredient"], State [] [])
-      , testCase "Get ingredient" $
-          stateTransition (State [] [Ingredient (WordName "Sugar") (Quantity 1) Cup]) (Get (WordName "Sugar")) @?= Right (["Got ingredient"], State [] [Ingredient (WordName "Sugar") (Quantity 1) Cup])
-      , testCase "Select ingredient" $
-          stateTransition emptyState (Select (Ingredient (WordName "Sugar") (Quantity 1) Cup)) @?= Right (["Selected ingredient"], State [] [Ingredient (WordName "Sugar") (Quantity 1) Cup])
-      , testCase "Update ingredient" $
-          stateTransition (State [] [Ingredient (WordName "Sugar") (Quantity 1) Cup]) (Update (WordName "Sugar") (WordName "Brown Sugar") (Quantity 2) Tbsp) @?= Right (["Updated ingredient"], State [] [Ingredient (WordName "Brown Sugar") (Quantity 2) Tbsp])
-      , testCase "Create ingredient list" $
-          stateTransition emptyState (CreateList (WordName "Groceries")) @?= Right (["Created list"], State [(WordName "Groceries", [])] [])
-      , testCase "Delete ingredient list" $
-          stateTransition (State [(WordName "Groceries", [])] []) (Delete (WordName "Groceries")) @?= Right (["Deleted list"], State [] [])
-      ]
+unitTests = testGroup "Parser Functions"
+  [ testCase "parseName - WordName" $
+      Lib2.parseName "apple" @?= Right (Lib2.WordName "apple", "")
+  
+  , testCase "parseName - NumberName" $
+      Lib2.parseName "123" @?= Right (Lib2.NumberName 123, "")
+  
+  , testCase "parseQuantity - valid quantity" $
+      Lib2.parseQuantity " 42" @?= Right (Lib2.Quantity 42, "")
+  
+  , testCase "parseQuantity - invalid quantity" $
+      Lib2.parseQuantity "abc" @?= Left "not a number"
+  
+  , testCase "parseUnit - valid unit" $
+      Lib2.parseUnit "cup" @?= Right (Lib2.Cup, "")
+  
+  , testCase "parseUnit - invalid unit" $
+      Lib2.parseUnit "invalid" @?= Left "Invalid unit"
+  
+  , testCase "parseQuery - Create query" $
+      Lib2.parseQuery "create(apple, 42, cup)" @?= Right (Lib2.Create (Lib2.WordName "apple") (Lib2.Quantity 42) Lib2.Cup)
+  
+  , testCase "parseQuery - Add query" $
+      Lib2.parseQuery "add(apple)" @?= Right (Lib2.Add (Lib2.WordName "apple"))
+  
+  , testCase "parseQuery - Remove query" $
+      Lib2.parseQuery "remove(apple)" @?= Right (Lib2.Remove (Lib2.WordName "apple"))
+  ]
+
+-- State Transition Tests
+stateTransitionTests :: TestTree
+stateTransitionTests = testGroup "State Transition"
+  [ testCase "stateTransition - Create Ingredient" $
+      let state = Lib2.emptyState
+      in Lib2.stateTransition state (Lib2.Create (Lib2.WordName "apple") (Lib2.Quantity 42) Lib2.Cup) @?= Right (["Created ingredient"], Lib2.State [] [Lib2.Ingredient (Lib2.WordName "apple") (Lib2.Quantity 42) Lib2.Cup])
+
+  , testCase "stateTransition - Add existing ingredient" $
+      let state = Lib2.State [] [Lib2.Ingredient (Lib2.WordName "apple") (Lib2.Quantity 42) Lib2.Cup]
+      in Lib2.stateTransition state (Lib2.Add (Lib2.WordName "apple")) @?= Right (["Added ingredient"], Lib2.State [] [Lib2.Ingredient (Lib2.WordName "apple") (Lib2.Quantity 42) Lib2.Cup, Lib2.Ingredient (Lib2.WordName "apple") (Lib2.Quantity 42) Lib2.Cup])
+
+  , testCase "stateTransition - Add non-existing ingredient" $
+      let state = Lib2.State [] [Lib2.Ingredient (Lib2.WordName "banana") (Lib2.Quantity 1) Lib2.Cup]
+      in Lib2.stateTransition state (Lib2.Add (Lib2.WordName "apple")) @?= Left "Ingredient not found"
+  
+  , testCase "stateTransition - Remove Ingredient" $
+      let state = Lib2.State [] [Lib2.Ingredient (Lib2.WordName "apple") (Lib2.Quantity 42) Lib2.Cup]
+      in Lib2.stateTransition state (Lib2.Remove (Lib2.WordName "apple")) @?= Right (["Removed ingredient"], Lib2.State [] [])
+  
+  , testCase "stateTransition - Get existing ingredient" $
+      let state = Lib2.State [] [Lib2.Ingredient (Lib2.WordName "apple") (Lib2.Quantity 42) Lib2.Cup]
+      in Lib2.stateTransition state (Lib2.Get (Lib2.WordName "apple")) @?= Right (["Got ingredient"], Lib2.State [] [Lib2.Ingredient (Lib2.WordName "apple") (Lib2.Quantity 42) Lib2.Cup])
+
+  , testCase "stateTransition - Get non-existing ingredient" $
+      let state = Lib2.State [] [Lib2.Ingredient (Lib2.WordName "banana") (Lib2.Quantity 1) Lib2.Cup]
+      in Lib2.stateTransition state (Lib2.Get (Lib2.WordName "apple")) @?= Left "Ingredient not found"
+  ]
+
+-- Additional parser tests for helper functions
+parserTests :: TestTree
+parserTests = testGroup "Parser Helper Functions"
+  [ testCase "parseIngredient - valid ingredient" $
+      Lib2.parseIngredient "apple: 42 cloves" @?= Right (Lib2.Ingredient (Lib2.WordName "apple") (Lib2.Quantity 42) Lib2.Cloves, "")
+  
+  , testCase "parseIngredientList - valid ingredient list" $
+      Lib2.parseIngredientList "apple: {apple: 42 cup, banana: 100 g}" @?= Right (Lib2.IngredientList (Lib2.WordName "apple") [Lib2.Ingredient (Lib2.WordName "apple") (Lib2.Quantity 42) Lib2.Cup, Lib2.Ingredient (Lib2.WordName "banana") (Lib2.Quantity 100) Lib2.G] [], "")
   ]
